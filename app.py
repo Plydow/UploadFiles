dd3c22af88c05bc30289c7813594f12c6c9b9857
Pierre LEGRAND <pierre.legrand@etu.utc.fr>
2024-02-26 17:23:46 +0100

    Fix app.py

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from routes.views import views
 
 app = Flask(__name__)
 
-def load_config(app, config_file='config.yaml'):
+def load_config(app, config_file='config/config.yaml'):
     with open(config_file) as yaml_file:
         cfg = yaml.safe_load(yaml_file)
     app.config.update(cfg)
 
-if __name__ == '__main__':
+def create_app():
     load_config(app)
     app.register_blueprint(views)
-    app.run()
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=False, host='0.0.0.0', port=5000)
